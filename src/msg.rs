6a5e2e6cb2681925b748c67f98d687f101ee60c3
Jakub Bogucki <jakub@moonbite.space>
2023-04-07 13:50:14 +0200

    Design all required messages

--- src/msg.rs
+++ src/msg.rs
@@ -1,11 +1,45 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Decimal;
+
+use crate::state::{Asset, Deposit, ID};
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub fee_address: String,
+    pub optional_service_fee: Decimal,
+}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    /// Deposit native tokens with an offer
+    Deposit {
+        exchange: Asset,
+        from: Option<String>,
+    },
+    /// Withdraw a deposit
+    /// If no ID specified, all sender's deposits will be withdrawn
+    Withdraw { id: Option<ID> },
+    /// Accepts exchange offer of given ID, executing the transaction
+    AcceptExchange { deposit_id: ID },
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    /// Query all deposits from address
+    #[returns(DepositsBySenderResponse)]
+    DepositsBySender { address: String },
+    /// Query one deposit using only its ID
+    #[returns(DepositByIdResponse)]
+    DepositById { id: ID },
+}
+
+#[cw_serde]
+pub struct DepositsBySenderResponse {
+    deposits: Vec<(ID, Deposit)>,
+}
+
+#[cw_serde]
+pub struct DepositByIdResponse {
+    deposit: Deposit,
+}
--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,5 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Addr, Order, StdResult, Storage, Uint128};
+use cosmwasm_std::{Addr, Decimal, Order, StdResult, Storage, Uint128};
 use cw_storage_plus::{Item, Map};
 
 #[cw_serde]
@@ -31,12 +31,17 @@ pub type ID = u64;
 pub const ID_COUNT: Item<ID> = Item::new("id_count");
 
 pub fn next_id(store: &mut dyn Storage) -> StdResult<ID> {
-    let id: ID = ID_COUNT.may_load(store)?.unwrap_or_default() + 1;
-    ID_COUNT.save(store, &id)?;
+    let id = ID_COUNT.may_load(store)?.unwrap_or_default();
+    ID_COUNT.save(store, &(id + 1))?;
     Ok(id)
 }
 
 pub const DEPOSITS: Map<(&Addr, ID), Deposit> = Map::new("deposits");
+
+pub fn add_deposit(storage: &mut dyn Storage, sender: &Addr, deposit: &Deposit) -> StdResult<()> {
+    let id = next_id(storage)?;
+    DEPOSITS.save(storage, (sender, id), &deposit)
+}
 
 pub fn get_deposits(storage: &dyn Storage, address: &Addr) -> StdResult<Vec<Deposit>> {
     DEPOSITS
@@ -48,3 +53,11 @@ pub fn get_deposits(storage: &dyn Storage, address: &Addr) -> StdResult<Vec<Depo
         })
         .collect::<StdResult<Vec<Deposit>>>()
 }
+
+#[cw_serde]
+pub struct FeeConfig {
+    pub fee_address: String,
+    pub optional_service_fee: Decimal,
+}
+
+pub const FEE_CONFIG: Item<FeeConfig> = Item::new("fee_config");
